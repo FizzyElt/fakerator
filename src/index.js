841cbe6cf9415af386b5b25397db7aaf10398899
FizzyElt <fizzyelt.dev@outlook.com>
2024-03-24 03:52:11 +0800

    add tuple select custom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { faker } from '@faker-js/faker';
 
-const test = {
-  type: 'obj',
-  props: [
-    ['subArr', { type: 'arr', item: { type: 'num', min: 100, max: 1000 }, len: 10 }],
-    ['n1', { type: 'num', min: 0, max: 50 }],
-    ['n2', { type: 'obj', props: [['f1', { type: 'num', max: 100 }]] }],
-  ],
+const generateCustomRandomFn = (config) => {
+  return config.generateFn;
 };
 
 const generateRandomNumFn = (config) => {
   const { min = 0, max } = config;
   return () => faker.number.int({ min, max });
+};
+
+const generateRandomSelectFn = (config) => {
+  const { items } = config;
+  return () => items[faker.number.int(items.length - 1)];
 };
 
 const generateRandomObjFn = (config) => {
@@ -31,6 +31,14 @@ const generateRandomArrayFn = (config) => {
   return () => Array.from({ length: config.len ?? 0 }, itemGeneratorFn);
 };
 
+const generateRandomTupleFn = (config) => {
+  const { configItems } = config;
+
+  const itemsFns = configItems.map(generateRandomFn);
+
+  return () => itemsFns.map((generateFn) => generateFn());
+};
+
 const generateRandomFn = (config) => {
   switch (config.type) {
     case 'obj':
@@ -39,11 +47,35 @@ const generateRandomFn = (config) => {
       return generateRandomArrayFn(config);
     case 'num':
       return generateRandomNumFn(config);
+    case 'select':
+      return generateRandomSelect(config);
+    case 'tuple':
+      return generateRandomTupleFn(config);
+    case 'custom':
+      return generateCustomRandomFn(config);
     default:
-      throw Error('no match type');
+      throw Error(`${config.type} is not supported type`);
   }
 };
 
-const randomFn = generateRandomFn(test);
+const test = {
+  type: 'obj',
+  props: [
+    ['subArr', { type: 'arr', item: { type: 'num', min: 100, max: 1000 }, len: 10 }],
+    ['n1', { type: 'num', min: 0, max: 50 }],
+    ['n2', { type: 'obj', props: [['f1', { type: 'num', max: 100 }]] }],
+  ],
+};
+
+const test2 = {
+  type: 'tuple',
+  configItems: [
+    { type: 'custom', generateFn: () => faker.number.int({ min: 0, max: 10 }) },
+    { type: 'custom', generateFn: () => faker.number.int({ min: 11, max: 20 }) },
+    { type: 'custom', generateFn: () => faker.number.int({ min: 21, max: 30 }) },
+  ],
+};
+
+const randomFn = generateRandomFn(test2);
 
 console.log(randomFn());
