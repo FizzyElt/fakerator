4d20b54c030604432a295345616b0fbdb5558c36
FizzyElt <fizzyelt.dev@outlook.com>
2024-04-02 01:14:19 +0800

    split code

---
+++ src/create_config.js
@@ -0,0 +1,14 @@
+// value
+export const createValueConfig = (generateFn) => ({ type: 'value', generateFn });
+
+// selection
+export const createSelectionConfig = (items) => ({ type: 'select', items });
+
+// object
+export const createObjectConfig = (content) => ({ type: 'obj', content });
+
+// array
+export const createArrayConfig = (item, len) => ({ type: 'arr', item, len });
+
+// tuple
+export const createTupleConfig = (configItems) => ({ type: 'tuple', configItems });
--- src/create_generator_fn.js
+++ src/create_generator_fn.js
@@ -1,69 +1,98 @@
-// value
-export const createValueConfig = (generateFn) => ({ type: 'value', generateFn });
+import { isNonEmptyArray } from 'effect/ReadonlyArray';
+import { isFunction } from 'effect/Function';
+import { isObject, isNumber } from 'effect/Predicate';
 
-export const createValueGenerator = (config) => {
-  return config.generateFn;
+// value
+export const createValueGenerator = (config, level = 0) => {
+  if (isFunction(config?.generateFn)) {
+    return config.generateFn;
+  }
+
+  throw new Error(`level ${level} value config is invalid \n${config}`);
 };
 
 // selection
-export const createSelectionConfig = (items) => ({ type: 'select', items });
+export const createSelectionGenerator = (config, level = 0) => {
+  if (isNonEmptyArray(config?.items)) {
+    const { items } = config;
 
-export const createSelectionGenerator = (config) => {
-  const { items } = config;
-  return () => items[faker.number.int(items.length - 1)];
+    return () => items[faker.number.int(items.length - 1)];
+  }
+
+  throw new Error(`level ${level} selection config is invalid \n${config}`);
 };
 
 // object
-export const createObjectConfig = (content) => ({ type: 'obj', content });
+export const createObjectGenerator = (config, level = 0) => {
+  if (isObject(config.content)) {
+    const keyWithFns = Object.entries(config.content).map(([key, subConfig]) => [
+      key,
+      createGeneratorByType(subConfig, level + 1),
+    ]);
 
-export const createObjectGenerator = (config) => {
-  const keyWithFns = Object.entries(config.content).map(([key, subConfig]) => [
-    key,
-    createGeneratorByType(subConfig),
-  ]);
+    return () => {
+      const result = {};
+      for (const [key, generateFn] of keyWithFns) {
+        result[key] = generateFn();
+      }
+      return result;
+    };
+  }
 
-  return () => {
-    const result = {};
-    for (const [key, generateFn] of keyWithFns) {
-      result[key] = generateFn();
-    }
-    return result;
-  };
+  throw new Error(`level ${level} object config is invalid \n${config}`);
 };
 
 // array
-export const createArrayConfig = (item, len) => ({ type: 'arr', item, len });
+export const createArrayGenerator = (config, level = 0) => {
+  if (isObject(config?.item)) {
+    const itemGeneratorFn = createGeneratorByType(config.item, level + 1);
 
-export const createArrayGenerator = (config) => {
-  const itemGeneratorFn = createGeneratorByType(config.item);
-  return () => Array.from({ length: config.len ?? 0 }, itemGeneratorFn);
+    return () => Array.from({ length: config.len ?? 0 }, itemGeneratorFn);
+  }
+
+  throw new Error(`level ${level} array config is invalid \n${config}`);
 };
 
 // tuple
-export const createTupleConfig = (configItems) => ({ type: 'tuple', configItems });
-
 export const createTupleGenerator = (config) => {
-  const { configItems } = config;
+  if (isNonEmptyArray(config?.configItems)) {
+    const itemsFns = config.configItems.map(createGeneratorByType);
 
-  const itemsFns = configItems.map(createGeneratorByType);
+    return () => itemsFns.map((generateFn) => generateFn());
+  }
 
-  return () => itemsFns.map((generateFn) => generateFn());
+  throw new Error(`level ${level} tuple config is invalid \n${config}`);
+};
+
+// TODO
+export const createBoundedSeriesGenerator = (config, level = 0) => {
+  if (!isNumber(config.upperLimit) || !isNumber(config.lowerLimit)) {
+    throw new Error('upperLimit and lowerLimit must be a number');
+  }
+
+  if (config.upperLimit <= config.lowerLimit) {
+    throw new Error('lowerLimit can not greater then upperLimit');
+  }
+
+  if (!isFunction(config.createBaseValue)) {
+    throw new Error('createBaseValue is not a function');
+  }
 };
 
 // all
-export const createGeneratorByType = (config) => {
+export const createGeneratorByType = (config, level = 0) => {
   switch (config.type) {
     case 'obj':
-      return createObjectGenerator(config);
+      return createObjectGenerator(config, level);
     case 'arr':
-      return createArrayGenerator(config);
+      return createArrayGenerator(config, level);
     case 'select':
-      return createSelectionGenerator(config);
+      return createSelectionGenerator(config, level);
     case 'tuple':
-      return createTupleGenerator(config);
+      return createTupleGenerator(config, level);
     case 'value':
-      return createValueGenerator(config);
+      return createValueGenerator(config, level);
     default:
-      throw Error(`${config.type} is not supported type`);
+      throw Error(`level ${level} config type "${config.type}" is not supported`);
   }
 };
